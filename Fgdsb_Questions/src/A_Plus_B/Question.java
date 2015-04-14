79490bc7df4d131f4592e505d5c4c76348d662ef
lixiangcliff <lixiang.cliff@gmail.com>
2015-04-13 22:45:54 -0700

    initial push

---
+++ Fgdsb_Questions/src/A_Plus_B/Question.java
@@ -0,0 +1,22 @@
+package A_Plus_B;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * Write a function to calculate A + B without using + - / * %.
+		Note: A and B are both 32-bit nonnegative integers, the output is guaranteed not to overflow.
+	 */
+	
+    public int plus_num(int a, int b) {
+       return 0; 
+    }
+
+}
