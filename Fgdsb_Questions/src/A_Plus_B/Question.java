75ca1ac80a0b22e35206b8b1d8879ba7c57289de
lixiangcliff <lixiang.cliff@gmail.com>
2015-04-14 11:49:53 -0700

    add A_Plus_B

--- Fgdsb_Questions/src/A_Plus_B/Question.java
+++ Fgdsb_Questions/src/A_Plus_B/Question.java
@@ -7,7 +7,8 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Question q = new Question();
+		System.out.println(q.plus_num(14545, 4546));
 	}
 	
 	/**
@@ -16,7 +17,18 @@ public class Question {
 	 */
 	
     public int plus_num(int a, int b) {
-       return 0; 
+    	int res = 0;
+    	int carry = 0;
+    	for (int i = 0; i < 32; i++) {
+    		int aBit = (a >> i) & 1;
+    		int bBit = (b >> i) & 1;
+    		int carryTemp = aBit == 1 && bBit == 1 ? 1 : 0;
+    		int sumTemp = (aBit ^ bBit) == 1 ? 1 : 0;
+    		int sum = (carry ^ sumTemp) == 1 ? 1 : 0;
+    		carry = carryTemp == 1 || (carry == 1 && sumTemp == 1) ? 1 : 0;
+    		res ^= (sum << i);
+    	}
+    	return res; 
     }
 
 }
