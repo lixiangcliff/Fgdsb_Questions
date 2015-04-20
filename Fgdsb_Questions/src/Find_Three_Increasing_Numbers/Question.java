2ebb8a023aabb81378722a172ee1d2caf1e7a65d
lixiangcliff <lixiang.cliff@gmail.com>
2015-04-19 23:39:51 -0700

    add Find_Three_Increasing_Numbers

---
+++ Fgdsb_Questions/src/Find_Three_Increasing_Numbers/Question.java
@@ -0,0 +1,23 @@
+package Find_Three_Increasing_Numbers;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * Given an array find 3 elements such that a[i] < a[j] < a[k] and i < j < k in O(N) time.
+	 * Your function should return indices of the three numbers. 
+	 * There might be multiple increasing subsequences, you only need to return any of them. Return [-1,-1,-1] if no result can be found.
+	 */
+	
+    public int[] three_increasing_nums(int[] nums) {
+    	
+    }
+
+}
