16ce5b77ba830260676ddc74b2b8233798c7a12d
lixiangcliff <lixiang.cliff@gmail.com>
2015-05-05 23:58:26 -0500

    add Longest_Increasing_Sequence_in_MatrixLongest_Increasing_Sequence_in_Matrix

---
+++ Fgdsb_Questions/src/Longest_Increasing_Sequence_in_Matrix/Question.java
@@ -0,0 +1,35 @@
+package Longest_Increasing_Sequence_in_Matrix;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/**
+		 * Write a function to find the longest increasing(increasing means one step) sequence in an integer matrix in 4 directions.
+
+			For example:
+			
+			[
+			  [1, 2, 3, 4]
+			  [8, 7, 6, 5]
+			]
+			should return:
+			
+			[1, 2, 3, 4, 5, 6, 7, 8] 
+			
+			[
+			  [1, 2, 3, 9]
+			  [8, 7, 6, 5]
+			]
+			should return:
+			
+			[5, 6, 7, 8]
+		 */
+
+	}
+
+}
