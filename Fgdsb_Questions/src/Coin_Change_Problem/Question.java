3b981b0bb1a162b661bafeabac76c60f80098cdb
lixiangcliff <lixiang.cliff@gmail.com>
2015-05-06 16:24:53 -0500

    add Coin_Change_Problem

---
+++ Fgdsb_Questions/src/Coin_Change_Problem/Question.java
@@ -0,0 +1,28 @@
+package Coin_Change_Problem;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * Given a set of currency denominations, find the minimum number of coins needed to represent an amount.
+	 * Return 0 if you can't use your coins to represent the target amount.
+	 * For example:
+	 * Suppose you have two kinds of coins: 1, 2. In order to represent 3, you have two options:
+	 * 1 + 1 + 1 = 3 or
+	 * 
+	 * 1 + 2 = 3
+	 * So the minimum number of coins is 2.
+	 */
+	
+    public int min_coins(int[] coins, int target) {
+    	return 0;
+    }
+
+}
