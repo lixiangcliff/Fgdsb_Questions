8aac70aabdf36105a1424c202d29240b965f18a7
lixiangcliff <lixiang.cliff@gmail.com>
2015-05-06 21:56:42 -0500

    update Coin_Change_Problem

--- Fgdsb_Questions/src/Coin_Change_Problem/Question.java
+++ Fgdsb_Questions/src/Coin_Change_Problem/Question.java
@@ -1,5 +1,7 @@
 package Coin_Change_Problem;
 
+import java.util.Arrays;
+
 public class Question {
 
 	/**
@@ -7,7 +9,10 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] coins = {2, 5, 10};
+		int target = 1;
+		Question q = new Question();
+		System.out.println(q.min_coins(coins, target));
 	}
 	
 	/**
@@ -21,7 +26,28 @@ public class Question {
 	 * So the minimum number of coins is 2.
 	 */
 	
+	//there might be something wrong in test case
     public int min_coins(int[] coins, int target) {
+    	if (coins == null || coins.length == 0 || target <= 0) {
+    		return 0;
+    	}
+    	Arrays.sort(coins);
+    	int count = 0;
+    	int pos = coins.length - 1;
+    	while (target > 0) {
+    		while (target - coins[pos] >= 0) {
+    			target -= coins[pos];
+    			count++;
+    			if (target == 0) {
+        			return count;
+    			}
+    		}
+			if (pos == 0) {
+				break;
+			} else {
+				pos--;
+			}
+    	}
     	return 0;
     }
 
